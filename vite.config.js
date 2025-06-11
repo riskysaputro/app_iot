d298a52f9c64302a359a78cf13e7fa4ce3ebf23e
Risky Saputro <riskysaputroid@gmail.com>
2025-06-11 20:48:48 +0700

    config allow host

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 // import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss()],
+  plugins: [tailwindcss()],
+  preview: {
+    port: 8080,
+    host: '0.0.0.0',
+    allowedHosts: ['appiot-production.up.railway.app'], // Ganti sesuai domain kamu di Railway
+  },
 });
